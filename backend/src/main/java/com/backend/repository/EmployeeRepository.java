6f27ba410453d0123db5ee4c6a9f0ea22c0897d6
Mohamed Mohamed Ibrahim <121159820+Mohamed-Mohamed-Ibrahim@users.noreply.github.com>
2024-12-26 22:50:55 +0200

    connecting to db

--- backend/src/main/java/com/backend/repository/EmployeeRepository.java
+++ backend/src/main/java/com/backend/repository/EmployeeRepository.java
@@ -1,7 +1,7 @@
-package com.backend.repository;
-
-import com.backend.entity.Employee;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EmployeeRepository extends JpaRepository<Employee,Integer> {
-}
+//package com.backend.repository;
+//
+//import com.backend.entity.Employee;
+//import org.springframework.data.jpa.repository.JpaRepository;
+//
+//public interface EmployeeRepository extends JpaRepository<Employee,Integer> {
+//}
